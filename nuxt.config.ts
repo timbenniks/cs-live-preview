ab84b8a55cac418c8165e3f3aea4c2cd6e8aa574
Tim Benniks <tbenniks@gmail.com>
2024-09-11 10:30:01 +0200

    better versions

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,33 +10,6 @@ export default defineNuxtConfig({
     transpile: transpiles,
   },
 
-  modules: [],
-
-  security: {
-    headers: {
-      contentSecurityPolicy: {
-        'frame-ancestors': ["'self'", "https://eu-app.contentstack.com"],
-        'connect-src': ["'self'",
-          "*.csnonprod.com",
-          "*.contentstack.com",
-          "*.salesforce-sites.com",
-          "https://cdn-personalization.contentstack.com",
-          "https://cdn.contentstack.io",
-          "https://api.appcues.net",
-          "wss://api.appcues.net",
-          "https://liveagentcontentstack.secure.force.com",
-          "https://api-iam.intercom.io",
-          "wss://nexus-websocket-a.intercom.io",
-          "wss://ws-mt1.pusher.com",
-          "https://widget.usersnap.com",
-          "https://api.commandbar.com",
-          "https://t.commandbar.com",
-          "https://s3.us-west-2.amazonaws.com",
-          "https://*.browser-intake-datadoghq.eu"]
-      },
-    },
-  },
-
   runtimeConfig: {
     public: {
       apiKey: process.env.CONTENTSTACK_API_KEY,
@@ -45,6 +18,4 @@ export default defineNuxtConfig({
       managementToken: process.env.CONTENTSTACK_MANAGEMENT_TOKEN
     },
   },
-
-  modules: ["nuxt-security"],
 })
